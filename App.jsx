7c03ec59ba163a9f32ffee63002a398deb4350d7
DagoTshering <dagotshering777@gmail.com>
2024-12-20 18:38:21 +0600

    components

--- App.jsx
+++ App.jsx
@@ -1,9 +1,11 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button } from 'react-native' // we want to import multiple components from a module, we have to use {} bracket
 import React from 'react'
+import First from './src/components/First'
 
 const App = () => {
   return (
     <View>
+      <First/>
       <Text style={{fontSize: 30}}>This is my first React App</Text>
       <Button title='Press Me'/>
     </View>
